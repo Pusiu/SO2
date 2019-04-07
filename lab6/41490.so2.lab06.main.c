e66b0405be1cc0f7a4f92fab37a3104c14a9acbf
Gracjan Puch <gracjanpuszek@gmail.com>
2019-04-07 20:35:03 +0200

    Mmap added. Reading lines from file needs bugfixing though.

--- lab6/41490.so2.lab06.main.c
+++ lab6/41490.so2.lab06.main.c
@@ -1,7 +1,7 @@
 // SO2 IS1 222B LAB06
 // Gracjan Puch
 // pg41490@zut.edu.pl
-// gcc -lpthread -lcrypt 41490.so2.lab06.main.c -o lab06 
+// gcc 41490.so2.lab06.main.c -o lab06 -lpthread -lcrypt  
 //  ./lab06 $6$5MfvmFOaDU$CVt7jU9wJRYz3K98EklAJqp8RMG5NvReUSVK7ctVvc2VOnYVrvyTfXaIgHn2xQS78foEJZBq2oCIqwfdNp.2V1 ./small 4
 
 #define _GNU_SOURCE
@@ -12,7 +12,21 @@
 #include <stdlib.h>
 #include <crypt.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdbool.h>
 
+
+bool foundSolution=false;
+char* passwordsFile="small.txt";
+char* offset = NULL;
+unsigned int filesize = 0;
+char* mappedFile = NULL;
+char* fullHash = NULL;
+char cryptMethod = '6';
+char* salt = NULL;
+char* hash = NULL;
 
 int GetCharIndex(char* const source, char character, int occurance)
 {
@@ -30,14 +44,58 @@ int GetCharIndex(char* const source, char character, int occurance)
     return -1;
 }
 
+
+char* ReadLine()
+{
+    char* newline=memchr(offset, '\n', 1024);
+    if (newline == NULL)
+    {
+        printf("readline null\n");
+        return NULL;
+    }
+    int length = (unsigned int)(newline-offset);
+    printf("Character: %d", newline);
+    printf(" at %d", length);
+
+    char* output = malloc(length+2);
+    memcpy(output, offset, length);
+    //output[length]='\0';
+    offset=newline+1;
+    printf("Returning: %s", output);
+    return output;
+}
+
+void Crack()
+{
+    struct crypt_data data;
+    data.initialized = 0;
+    while (!foundSolution)
+    {
+        char* line = ReadLine();
+        if (line == NULL)
+            {
+                free(line);
+                break;
+            }
+        printf("Read line: %s", line);
+        char* hash = crypt_r(line, salt, &data);
+        printf("Trying to crack: %s\nHash:%s", line, hash);
+        
+        if (hash == fullHash)
+        {
+            foundSolution=true;
+            printf("Solution found!\nOriginal password: %s", line);
+        }
+
+        free(line);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     int numOfThreads =  sysconf(_SC_NPROCESSORS_ONLN);
 
-    char* fullHash = NULL;
-    char cryptMethod = '6';
-    char* salt = NULL;
-    char* hash = NULL;
+
 
     if (argc >= 2)
     {
@@ -53,13 +111,23 @@ int main(int argc, char* argv[])
 
     struct crypt_data data;
     data.initialized = 0;
+
+    struct stat st; //stat for getting file size
+    stat(passwordsFile,&st);
+    int fd = open(passwordsFile, O_RDONLY,0);
+    mappedFile = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    offset=mappedFile;
     
-    char* crypted = crypt_r("aarek1940", "$6$5MfvmFOaDU$", &data);
-    printf("%d\n%c\n%s\n%s\n%s\n",numOfThreads,cryptMethod, salt, hash, crypted);
+    Crack();
+
+    //char* crypted = crypt_r("aarek1940", "$6$5MfvmFOaDU$", &data);
+    //printf("%d\n%c\n%s\n%s\n%s\n",numOfThreads,cryptMethod, salt, hash, crypted);
     if (argc >= 2)
     {
         free(fullHash);
         free(salt);
         free(hash);
     }
+    munmap(mappedFile, st.st_size);
+    close(fd);
 }
